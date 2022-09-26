84bf4a52e2bc185f617eb5363dbfdbccdd80ff9a
Chakraborty <sabbyasachi.chakraborty@capgemini.com>
2022-09-26 12:02:24 +0530

    Order updated

---
+++ src/main/java/com/sprint/ecommerce/entity/Order.java
@@ -0,0 +1,27 @@
+package com.sprint.ecommerce.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Order {
+	@Id
+	private int order_id;
+	@OneToOne
+	private Customer customer;
+	@OneToOne
+	private Seller seller;
+	@ManyToMany
+	private List<Product> product;
+}
