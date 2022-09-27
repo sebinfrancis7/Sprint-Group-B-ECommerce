10ad60a07371bfcf8760e0aecaf0a5483a4b35ec
Sharma <madhur.sharma@capgemini.com>
2022-09-27 09:40:55 +0530

    feedback mapping

--- src/main/java/com/sprint/ecommerce/entity/Feedback.java
+++ src/main/java/com/sprint/ecommerce/entity/Feedback.java
@@ -1,19 +1,28 @@
 package com.sprint.ecommerce.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Feedback {
 
+	@Id
 	private int feedbackId;
+	@OneToOne
 	private Customer customer;
+	@OneToOne
 	private Seller seller;
+	@OneToOne
 	private Product product;
 	private int rating;
 	private String feedback;
