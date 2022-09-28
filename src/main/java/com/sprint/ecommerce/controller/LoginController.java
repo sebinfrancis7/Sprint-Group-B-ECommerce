08e7bc1ebff2a2a585067082f8a296524049f3ae
Sharma <madhur.sharma@capgemini.com>
2022-09-28 11:04:44 +0530

    seller login

--- src/main/java/com/sprint/ecommerce/controller/LoginController.java
+++ src/main/java/com/sprint/ecommerce/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sprint.ecommerce.entity.Customer;
+import com.sprint.ecommerce.entity.Seller;
 import com.sprint.ecommerce.exception.NotFoundException;
 import com.sprint.ecommerce.service.CustomerService;
 import com.sprint.ecommerce.service.SellerService;
@@ -24,6 +25,12 @@ public class LoginController {
 	@PostMapping("/login/customer")
 	public ResponseEntity<String> loginCustomer(@RequestBody Customer customer) throws NotFoundException {
 		String response = customerServ.loginCustomer(customer);
+		return new ResponseEntity<String>(response, HttpStatus.OK);
+	}
+	
+	@PostMapping("/login/seller")
+	public ResponseEntity<String> loginSeller(@RequestBody Seller seller) throws NotFoundException {
+		String response = sellerServ.loginSeller(seller);
 		return new ResponseEntity<String>(response, HttpStatus.OK);
 	}
 }
