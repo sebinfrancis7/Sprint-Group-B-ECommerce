c347535334bce6e374b4276f7bdbbe9ff60c5650
sebinfrancis7 <sebin.k.francis07@gmail.com>
2022-10-02 21:40:01 +0530

    test cases

--- src/test/java/com/sprint/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/sprint/ecommerce/EcommerceApplicationTests.java
@@ -2,25 +2,44 @@ package com.sprint.ecommerce;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.sprint.ecommerce.entity.Customer;
 import com.sprint.ecommerce.entity.Product;
 import com.sprint.ecommerce.entity.Seller;
-import com.sprint.ecommerce.repository.ProductRepository;
-import com.sprint.ecommerce.repository.SellerRepository;
+import com.sprint.ecommerce.exception.AlreadyExistsException;
+import com.sprint.ecommerce.exception.NotFoundException;
+import com.sprint.ecommerce.repository.OrdersRepository;
+import com.sprint.ecommerce.service.CustomerService;
+import com.sprint.ecommerce.service.FeedbackService;
+import com.sprint.ecommerce.service.OrdersService;
+import com.sprint.ecommerce.service.ProductService;
+import com.sprint.ecommerce.service.SellerService;
 
 @SpringBootTest
 class EcommerceApplicationTests {
 
 	@Autowired
-	ProductRepository productRepo;
+	ProductService prodServ;
 
 	@Autowired
-	SellerRepository sellerRepo;
+	SellerService sellerServ;
+
+	@Autowired
+	CustomerService custServ;
+
+	@Autowired
+	OrdersService orderServ;
+
+	@Autowired
+	FeedbackService feedbackServ;
+
+	@Autowired
+	OrdersRepository ordersRepo;
 
 	@Test
 	void contextLoads() {
@@ -29,46 +48,101 @@ class EcommerceApplicationTests {
 	@Test
 	public void saveProductTest() {
 		Product p1 = new Product();
-		p1.setProdId(107);
+		p1.setProdId(1000);
 		p1.setProdName("iphone");
 		p1.setCategory("Electronics");
 		p1.setPrice(165000);
-		productRepo.save(p1);
+		try {
+			prodServ.saveProduct(p1);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-		Product savedProduct1 = productRepo.findById(105).get();
+		Product savedProduct1 = null;
+		try {
+			savedProduct1 = prodServ.getProductById(1000).get();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		assertEquals(p1, savedProduct1);
-//
-//		Product p2 = new Product();
-//		p2.setProdId(106);
-//		p2.setProdName("shoes");
-//		p2.setCategory("Apparel");
-//		p2.setPrice(5438);
-//		productRepo.save(p2);
-//
-//		Product savedProduct2 = productRepo.findById(106).get();
-//		assertEquals(p2, savedProduct2);
 	}
 
 	@Test
-	public void saveSellerTest() {
+	public void saveSellerTest() throws Exception {
 		Seller s = new Seller();
-		s.setSellerId(102);
+		s.setSellerId(1000);
 		s.setSellerName("Rohit Sharma");
-		s.setUserName("r_20asasd3");
+		s.setUserName("r_203");
 		s.setPassword("password");
-		Product p1 = productRepo.findById(105).get();
-//		Product p2 = productRepo.findById(106).get();
-//		List<Product> list = List.of(p1, p2);
-		List<Product> list = List.of(p1);
-		s.setProduct(list);
 
-		sellerRepo.save(s);
+		sellerServ.saveSeller(s);
 
-		Seller savedSeller = sellerRepo.findById(102).get();
+		Seller savedSeller = sellerServ.getSellerById(1000).get();
 
 		System.out.println(s.getSellerId());
 		System.out.println(savedSeller.getSellerId());
 		assertEquals(s, savedSeller);
 	}
+
+	@Transactional
+	@Rollback(false)
+	@Test
+	public void addProductToSellerTest() throws NotFoundException, AlreadyExistsException, Exception {
+		Product p2 = new Product();
+		p2.setProdId(1001);
+		p2.setProdName("shoes");
+		p2.setCategory("Apparel");
+		p2.setPrice(5438);
+
+		String resp = sellerServ.addToProductList(1000, p2);
+		assertEquals("Product added successfully", resp);
+	}
+
+	@Test
+	public void saveCustomerTest() throws Exception {
+		Customer c = new Customer();
+		c.setCustId(1000);
+		c.setCustName("rahul mehta");
+		c.setUserName("rm07");
+		c.setPassword("password");
+
+		custServ.addCustomer(c);
+
+		Customer savedCustomer = custServ.getCustomerById(1000).get();
+
+		assertEquals(c.getCustId(), savedCustomer.getCustId());
+	}
+
+//	@Test
+//	@Transactional
+//	@Rollback(false)
+//	public void placeOrderTest() throws Exception {
+//
+//		Orders o = new Orders();
+//		o.setOrderId(1000);
+//
+//		Seller s = new Seller();
+//		s.setSellerId(1000);
+//		Product p = new Product();
+//		p.setProdId(1001);
+//		Customer c = new Customer();
+//		c.setCustId(1000);
+//		o.setSeller(s);
+//		o.setProduct(p);
+//		o.setCustomer(c);
+//		o.setOrderDate(LocalDateTime.now());
+//		o.setDeliveryDate(LocalDate.now().plusDays(3));
+//		ordersRepo.saveAndFlush(o);
+//
+//	}
+//
+//	@Test
+//	@Transactional
+//	public void findPlacedOrder() throws NotFoundException {
+//		Orders savedOrder = orderServ.getOrderById(1000);
+//		assertEquals(1000, savedOrder.getOrderId());
+//	}
 
 }
